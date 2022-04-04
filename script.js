cb626ceb5d2b9d32ae8eead7af1a7cbb79b79cb6
edward29 <91964639+edward29@users.noreply.github.com>
2022-04-04 14:30:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
     const stopBtn = document.querySelector("#stopbtn");
     var audio = document.querySelector('#audio');
     var count = 0;
+    
     const q = prompt("Do you want to enter?\n(type \"yes\" if you want  \"no\" if you don't)");
 
     
